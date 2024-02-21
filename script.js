f4d08d816ed5e371e12168c9a470d2ab67a2c1d7
ValerianArdelean <valerian_ardelean@yahoo.com>
2024-02-21 19:58:50 +0200

    Initial commit.
    Created the code for displaying a functional semaphore button,
    using custom HTML, CSS and JS.

---
+++ script.js
@@ -0,0 +1,17 @@
+let colors = ["red", "yellow", "green"];
+let color = 1;
+let button = document.getElementById("button");
+button.addEventListener("click", changeColor);
+function changeColor() {
+    button.style.backgroundColor = colors[color];
+    color += 1;
+    resetInterval();
+    if (color > 2) {
+        color = 0;
+    }
+}
+let time = setInterval(changeColor, 10000);
+function resetInterval() {
+      clearInterval(time);
+      time = setInterval(changeColor, 10000);
+}
