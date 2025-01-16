03dc3879d4f0b3369ca1490e482360ad139400e9
ValerianArdelean <valerian_ardelean@yahoo.com>
2025-01-17 01:09:22 +0200

    Resolved formatting errors: spacing

--- script.js
+++ script.js
@@ -3,15 +3,16 @@ let color = 1;
 let button = document.getElementById("button");
 button.addEventListener("click", changeColor);
 function changeColor() {
-    button.style.backgroundColor = colors[color];
-    color += 1;
-    resetInterval();
-    if (color > 2) {
-        color = 0;
-    }
+	button.style.backgroundColor = colors[color];
+	color += 1;
+	resetInterval();
+	if (color > 2) {
+		color = 0;
+	}
 }
+
 let time = setInterval(changeColor, 10000);
 function resetInterval() {
-      clearInterval(time);
-      time = setInterval(changeColor, 10000);
+	clearInterval(time);
+	time = setInterval(changeColor, 10000);
 }
